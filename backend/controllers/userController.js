178bb3c7f933ff3d8b7ecaadccb4cea0404efa53
keyursaradva <saradvakeyur@gmail.com>
2024-05-23 09:50:42 +0530

    create getUserProfile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -135,4 +135,21 @@ const updateUser = async ( req, res) => {
     } 
 };
 
-export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser };
+const getUserProfile = async (req, res) => {
+    const { username } = req.params;
+    try {
+        const user = await User.findOne({username}).select("-password").select("-updatedAt");
+        if(!user){
+            res.status(400).json({message:"User not found"});
+        }else{
+            res.status(200).json(user);
+        }
+
+        
+    } catch (error) {
+        res.status(500).json({message:error.message});
+        console.log("Error in getUserProfile: " , error.message);
+    }
+};
+
+export { signupUser, loginUser, logoutUser, followUnFollowUser, updateUser , getUserProfile};
