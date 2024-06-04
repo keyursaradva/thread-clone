91af0ecd47d507d52eb958fbb07344474da86223
keyursaradva <saradvakeyur@gmail.com>
2024-06-04 18:15:05 +0530

    ChatPage design added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { Button, Container } from "@chakra-ui/react"
-import {Routes,Route, Navigate} from "react-router-dom";
-import UserPage  from './pages/UserPage';
+import { Box, Container } from "@chakra-ui/react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import UserPage from './pages/UserPage';
 import Header from "./components/Header";
 import PostPage from './pages/PostPage'
 import HomePage from "./pages/HomePage";
@@ -9,28 +9,33 @@ import { useRecoilValue } from "recoil";
 import userAtom from "./atoms/userAtom";
 import UpdateProfilePage from "./pages/UpdateProfilePage";
 import CreatePost from "./components/CreatePost";
+import ChatPage from "./pages/ChatPage";
+
 
 function App() {
   const user = useRecoilValue(userAtom);
-  return (  
-    <Container maxW="620px">
-      <Header />
-      <Routes>
-        <Route path="/" element={user ? <HomePage/> : <Navigate to="/auth" />}/>
-        <Route path="/auth" element={!user ? <AuthPage/> : <Navigate to="/" />}/>
-        <Route path="/update" element={user ? <UpdateProfilePage/> : <Navigate to="/auth" />}/>
-        <Route path="/:username" element={user ? (
-          <>
-          <UserPage />
-          <CreatePost />
-          </>
-        ):(
-          <UserPage />
-        )}/>
-        <Route path="/:username/post/:pid" element={<PostPage/>}/>
-      </Routes>
+  return (
+    <Box position={"relative"} w='full'>
+      <Container maxW="620px">
+        <Header />
+        <Routes>
+          <Route path="/" element={user ? <HomePage /> : <Navigate to="/auth" />} />
+          <Route path="/auth" element={!user ? <AuthPage /> : <Navigate to="/" />} />
+          <Route path="/update" element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />} />
+          <Route path="/:username" element={user ? (
+            <>
+              <UserPage />
+              <CreatePost />
+            </>
+          ) : (
+            <UserPage />
+          )} />
+          <Route path="/:username/post/:pid" element={<PostPage />} />
+          <Route path="/chat" element={user ? <ChatPage /> : <Navigate to={"/auth"} />} />
+        </Routes>
 
-    </Container>
+      </Container>
+    </Box>
   )
 }
 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,6 +7,7 @@ import { RxAvatar } from "react-icons/rx";
 import { FiLogOut } from "react-icons/fi";
 import useLogout from "../hooks/useLogout";
 import authScreenAtom from "../atoms/authAtom";
+import { BsFillChatQuoteFill } from "react-icons/bs";
 
 
 export default function Header() {
@@ -18,12 +19,12 @@ export default function Header() {
   return (
     <Flex justifyContent={"space-between"} mt={6} mb={12}>
       {user && (
-        <Link as={RouterLink} to={"/auth"} >
+        <Link as={RouterLink} to="/" >
           <AiFillHome size={24} />
         </Link>
       )}
       {!user && (
-        <Link as={RouterLink} to='/' 
+        <Link as={RouterLink} to={"/auth"} 
           onClick={() => setAuthScreen("login")}
         >
           Login
@@ -42,6 +43,9 @@ export default function Header() {
         <Flex alignItems={"center"} gap={4}>
           <Link as={RouterLink} to={`/${user.username}`} >
             <RxAvatar size={24} />
+          </Link>
+          <Link as={RouterLink} to={`/chat`}>
+            <BsFillChatQuoteFill size={20} />
           </Link>
           <Button
             size={"xm"}
